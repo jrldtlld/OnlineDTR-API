3ce02e4ef91519600b772f21ddf051682aeafe99
Jerald Talledo <jerald.talledo@g.msuiit.edu.ph>
2018-10-09 22:57:42 +0800

    added run.py for testing

---
+++ run.py
@@ -0,0 +1,8 @@
+from api import *
+import os
+
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True, threaded=True, port=5050)
+	
