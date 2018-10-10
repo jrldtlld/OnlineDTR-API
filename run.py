02d944ea42eb3e8ef54f586775da5b1fcf9aed3b
Jerald Talledo <jerald.talledo@g.msuiit.edu.ph>
2018-10-10 17:15:49 +0800

    added empty app, models and __init__

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from api import *
+from app import *
 import os
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
-    # app.run(debug=True, threaded=True, port=5050)
+	# port = int(os.environ.get("PORT", 5000))
+	# app.run(host='0.0.0.0', port=port)
+   app.run(debug=True, threaded=True, port=5050)
 	
