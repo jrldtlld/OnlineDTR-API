e371f708013798f68861b3d8741a711d2700e29e
Jerald Talledo <jerald.talledo@g.msuiit.edu.ph>
2018-10-18 02:25:41 +0800

    fixed typho ny modification

--- api/__init__.py
+++ api/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.debug = True
 dbase = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEM_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.urandom(24)
  
 from models import *
