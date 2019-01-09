739a4b338d8df17189d03897ab3052d5a11afa6d
Jerald Talledo <jerald.talledo@g.msuiit.edu.ph>
2019-01-09 11:59:17 +0800

    fixed summary error

--- api/app.py
+++ api/app.py
@@ -506,9 +506,8 @@ def summary(dates):
          employees.append(employee_data)
       return jsonify({'Employee': employees})
    else:
-      summary = Attendance.query.filter(and_(Attendance.employee_code == data['emp_id'],\
-       (extract('year', Attendance.attendance_date) == (dates.strftime("%Y"))),\
-         (extract('month', Attendance.attendance_date) == (dates.strftime("%m"))))).order_by(Attendance.attendance_date.desc()).all()
+      summary = Attendance.query.filter(Attendance.employee_code == data['emp_id']).extract('year', Attendance.attendance_date) == (dates.strftime("%Y")).\
+         extract('month', Attendance.attendance_date) == (dates.strftime("%m")).order_by(Attendance.attendance_date.desc()).all()
       employees = []
       if not summary:
          return jsonify({'Employee': employees})
